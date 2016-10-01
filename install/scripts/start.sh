5c9690c6c78f491dcb1a15fa3098a19178b0ecab
r2h2 <rainer@hoerbe.at>
2016-10-01 09:54:46 +0200

    eport static web contents

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -4,7 +4,7 @@
 
 # make static files available for reverse proxy
 mkdir -p /export/static
-cp -npr /opt/saml_schematron/saml_schtron/webapp/static/ /export/static
+cp -npr /opt/saml_schematron/saml_schtron/webapp/static/* /export/static/
 
 # start webserver
 start_srv.py > /var/log/access.log 2> /var/log/error.log
