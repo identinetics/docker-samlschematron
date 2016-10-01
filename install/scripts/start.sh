ac26286fe74824cdae1f309264b822549c6186a8
r2h2 <rainer@hoerbe.at>
2016-10-01 09:32:39 +0200

    eport static web contents

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -2,4 +2,9 @@
 #
 # Startup script for the saml schematron validation web app
 
+# make static files available for reverse proxy
+mkdir -p /export/static
+cp -npr /opt/saml_schematron/saml_schtron/webapp/static/ /export/static
+
+# start webserver
 start_srv.py > /var/log/access.log 2> /var/log/error.log
