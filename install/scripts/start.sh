57331db2a2b51a0e234f0659b7cc5597acdc5334
r2h2 <rainer@hoerbe.at>
2016-12-06 13:34:18 +0100

    fixes

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -4,7 +4,8 @@
 
 # make static files available for reverse proxy
 mkdir -p /export/static
-cp -npr /opt/saml_schematron/saml_schtron/webapp/static/* /export/static/
+cp -npr /opt/saml_schematron/saml_schtron/webapp/static/* /var/www/static/
 
 # start webserver
+source ./scripts/init.sh
 start_srv.py > /var/log/access.log 2> /var/log/error.log
